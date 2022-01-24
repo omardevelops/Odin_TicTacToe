c53c09d8eadc43e7936122f6361cecda26cd130d
Omar Ahmad <superomar121@gmail.com>
2022-01-24 17:27:45 +0400

    added gameboard module

--- tictactoe.js
+++ tictactoe.js
@@ -1,1 +1,12 @@
-console.log('chicken');
+const gameboard = (() => {
+    const gameboard = ['', 'x', 'o', '', 'x', 'o', 'x', 'o', 'x'];
+    const tickSpot = (symbol, index) => {
+        const spot = gameboard[index]; // retrieve spot to check if empty
+        if (spot === '' && index < gameboard.length) gameboard.splice(index, 1, symbol);
+    };
+    const logGameboard = () => console.log(gameboard);
+    return {tickSpot, logGameboard}
+})();
+
+gameboard.tickSpot('o', 4);
+gameboard.logGameboard();
