d80154f11654595ebf9419773498183912cb1d7c
Omar Ahmad <superomar121@gmail.com>
2022-01-25 20:44:55 +0400

    added: showing and hiding menus

--- tictactoe.js
+++ tictactoe.js
@@ -10,17 +10,24 @@ const gameboard = (() => {
 
 const displayController = (() => {
     const main_menu_div = document.getElementById('main_menu');
-    const hideMenu = () => {
-        main_menu_div.style.display = 'none';
+    const game_interface_div = document.getElementById('game_interface');
+    const hideElement = (element) => {
+        element.style.display = 'none';
+    };
+    const showElement = (element, displayType) => {
+        element.style.display = displayType;
     };
     const addEventListenersToButtons = () => {
         const buttons = Array.from(main_menu_div.getElementsByTagName('button'));
         buttons.forEach(button => {
-            button.addEventListener('click', hideMenu);
-            // logic for buttons based on ID (pvp vs player vs ai)
+            button.addEventListener('click', () => {
+                hideElement(main_menu_div);
+                showElement(game_interface_div, 'flex');
+            });
+        // logic for buttons based on ID (pvp vs player vs ai)
         });
     };
-    return {hideMenu, addEventListenersToButtons};
+    return {hideElement,showElement, addEventListenersToButtons};
 })();
 
 const Player = (name, symbol, isHuman) => {
@@ -30,8 +37,10 @@ const Player = (name, symbol, isHuman) => {
     return {getName, getSymbol, getIsHuman};
 };
 
-const startGame = () => {
-
+const startGame = (gamemode) => {
+    const player1 = ('Player 1', 'O', true);
+    const player2 = ('Player 2', 'X', true);
+    
 };
 
 // const john = Player('john', 'x');
