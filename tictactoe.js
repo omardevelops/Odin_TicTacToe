b30cbd663b60129d2ad53bc2a65cbb337f477b28
Omar Ahmad <superomar121@gmail.com>
2022-01-25 22:00:39 +0400

    interface control changes

--- tictactoe.js
+++ tictactoe.js
@@ -11,23 +11,26 @@ const gameboard = (() => {
 const displayController = (() => {
     const main_menu_div = document.getElementById('main_menu');
     const game_interface_div = document.getElementById('game_interface');
+
     const hideElement = (element) => {
         element.style.display = 'none';
     };
     const showElement = (element, displayType) => {
         element.style.display = displayType;
     };
+    const initInterface = () => {
+        hideElement(main_menu_div);
+        showElement(game_interface_div, 'flex');
+    };
     const addEventListenersToButtons = () => {
         const buttons = Array.from(main_menu_div.getElementsByTagName('button'));
         buttons.forEach(button => {
             button.addEventListener('click', () => {
-                hideElement(main_menu_div);
-                showElement(game_interface_div, 'flex');
+                startGame(button.id);
             });
-        // logic for buttons based on ID (pvp vs player vs ai)
         });
     };
-    return {hideElement,showElement, addEventListenersToButtons};
+    return {initInterface, addEventListenersToButtons};
 })();
 
 const Player = (name, symbol, isHuman) => {
@@ -38,13 +41,14 @@ const Player = (name, symbol, isHuman) => {
 };
 
 const startGame = (gamemode) => {
-    const player1 = ('Player 1', 'O', true);
-    const player2 = ('Player 2', 'X', true);
-    
+    displayController.initInterface();
+    let player1 = ('Player 1', 'O', true);
+    let player2;
+    if (gamemode === 'playerVSplayer') {
+        player2 = ('Player 2', 'X', true);
+    } else {
+        player2 = ('AI Player', 'X', false);
+    }    
 };
 
-// const john = Player('john', 'x');
-
-// gameboard.tickSpot('o', 3);
-// gameboard.logGameboard();
 displayController.addEventListenersToButtons();
